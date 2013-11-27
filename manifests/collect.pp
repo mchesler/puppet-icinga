845876162368cd0dc77f8dfe6e822858f3278460
Matt Chesler <mchesler@theladders.com>
2013-11-27 11:46:48 -0500

    Limit config to same environment as icinga host

--- manifests/collect.pp
+++ manifests/collect.pp
@@ -6,27 +6,27 @@ class icinga::collect {
 
   if $::icinga::server {
     # Set defaults for collected resources.
-    Nagios_host <<| |>>              { notify => Service[$::icinga::service_server] }
-    Nagios_service <<| |>>           { notify => Service[$::icinga::service_server] }
-    Nagios_hostextinfo <<| |>>       { notify => Service[$::icinga::service_server] }
-    Nagios_command <<| |>>           { notify => Service[$::icinga::service_server] }
-    Nagios_contact <<| |>>           { notify => Service[$::icinga::service_server] }
-    Nagios_contactgroup <<| |>>      { notify => Service[$::icinga::service_server] }
-    Nagios_hostdependency <<| |>>    { notify => Service[$::icinga::service_server] }
-    Nagios_hostescalation <<| |>>    { notify => Service[$::icinga::service_server] }
-    Nagios_hostgroup <<| |>>         {
+    Nagios_host <<| tag == "deployment::${::environment}" |>>              { notify => Service[$::icinga::service_server] }
+    Nagios_service <<| tag == "deployment::${::environment}" |>>           { notify => Service[$::icinga::service_server] }
+    Nagios_hostextinfo <<| tag == "deployment::${::environment}" |>>       { notify => Service[$::icinga::service_server] }
+    Nagios_command <<| |>>                                                 { notify => Service[$::icinga::service_server] }
+    Nagios_contact <<| tag == "deployment::${::environment}" |>>           { notify => Service[$::icinga::service_server] }
+    Nagios_contactgroup <<| tag == "deployment::${::environment}" |>>      { notify => Service[$::icinga::service_server] }
+    Nagios_hostdependency <<| tag == "deployment::${::environment}" |>>    { notify => Service[$::icinga::service_server] }
+    Nagios_hostescalation <<| tag == "deployment::${::environment}" |>>    { notify => Service[$::icinga::service_server] }
+    Nagios_hostgroup <<| tag == "deployment::${::environment}" |>>         {
       notify => Service[$::icinga::service_server],
       target => "${::icinga::targetdir}/hostgroups.cfg",
     }
-    Nagios_servicedependency <<| |>> { notify => Service[$::icinga::service_server] }
-    Nagios_serviceescalation <<| |>> { notify => Service[$::icinga::service_server] }
-    Nagios_serviceextinfo <<| |>>    { notify => Service[$::icinga::service_server] }
-    Nagios_servicegroup <<| |>>      { notify => Service[$::icinga::service_server] }
-    Nagios_timeperiod <<| |>>        {
+    Nagios_servicedependency <<| tag == "deployment::${::environment}" |>> { notify => Service[$::icinga::service_server] }
+    Nagios_serviceescalation <<| tag == "deployment::${::environment}" |>> { notify => Service[$::icinga::service_server] }
+    Nagios_serviceextinfo <<| tag == "deployment::${::environment}" |>>    { notify => Service[$::icinga::service_server] }
+    Nagios_servicegroup <<| tag == "deployment::${::environment}" |>>      { notify => Service[$::icinga::service_server] }
+    Nagios_timeperiod <<| |>>                                              {
       notify => Service[$::icinga::service_server],
       target => "${::icinga::targetdir}/timeperiods.cfg",
     }
-    Icinga::Downtime <<| |>> {  }
+    Icinga::Downtime <<| |>>                                               {  }
   }
 
   if $::icinga::client {
